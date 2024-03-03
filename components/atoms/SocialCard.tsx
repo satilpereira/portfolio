1dd7eaac235a6b1c238c78d1e928e58492cba180
satilpereira <satil.prog@gmail.com>
2024-03-02 21:42:50 -0300

    Add favicon

--- components/atoms/SocialCard.tsx
+++ components/atoms/SocialCard.tsx
@@ -16,7 +16,7 @@ const SocialCard: React.FC<
       href={url}
       {...rest}
       target='_blank'
-      rel='noopener noreferrer'
+      rel='external nofollow noreferrer noopener'
       className='flex items-center text-sm justify-start p-2 space-x-2 dark:bg-shark-700 text-shark-700 bg-zircon-200/30 hover:bg-zircon-300/30 dark:text-shark-300 rounded-md hover:dark:bg-shark-600 hover:dark:text-shark-200 transition-all duration-300 ease-in-out'
     >
       <div>{icon}</div>
